ca7822a6244da2bd0e76e265f938d5693bbbb936
Flávio Dutra <flaviomdutra@outlook.com>
2023-02-07 09:18:29 -0300

    feat: add table and fake api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createServer } from "miragejs";
+
 import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Freelancer de WebSite",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+      ];
+    });
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,16 +3,16 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 
     :root {
-        --background: #F0F2F5;
-        --red: #E62E4D;
+        --red: #E52E4D;
         --blue: #5429CC;
-        
+        --green: #33CC95;
         --blue-light: #6933FF;
-        
+
         --text-title: #363F5F;
         --text-body: #969CB3;
         
-        -shape: #FFFFFF;
+        --background: #f8f2f5;
+        --shape: #FFFFFF;
     }
 
     * {
